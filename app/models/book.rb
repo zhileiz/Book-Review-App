e6df447a73a647ffeb80e3cda166131993ec28e0
zhileiz <zhileiz@seas.upenn.edu>
2017-04-01 20:15:47 -0400

    added BootStrap

---
+++ app/models/book.rb
@@ -0,0 +1,22 @@
+class Book < ApplicationRecord
+  def create
+  end
+
+  def new
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+end
