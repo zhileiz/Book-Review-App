a7c3a23182fb50d47bffe77a6d8a7033a0178d6c
zhileiz <zhileiz@seas.upenn.edu>
2017-04-01 21:02:59 -0400

    Basic Scaffold Dissected

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,22 +1,2 @@
 class Book < ApplicationRecord
-  def create
-  end
-
-  def new
-  end
-
-  def index
-  end
-
-  def show
-  end
-
-  def update
-  end
-
-  def edit
-  end
-
-  def destroy
-  end
 end
