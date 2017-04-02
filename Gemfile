195b96133eab90ba2549f49ab9ca2c3105e2a08e
zhileiz <zhileiz@seas.upenn.edu>
2017-04-01 22:14:45 -0400

    User System Complete

--- Gemfile
+++ Gemfile
@@ -36,6 +36,8 @@ gem 'simple_form', '~> 3.4'
 
 gem 'bootstrap-sass', '~> 3.3', '>= 3.3.7'
 
+gem 'devise', '~> 4.2', '>= 4.2.1'
+
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,11 +2,11 @@ class BooksController < ApplicationController
   before_action :find_book, only: [:show, :edit, :update, :destroy]
 
   def new
-    @book = Book.new
+    @book = current_user.books.new
   end
 
   def create
-    @book = Book.new(book_params)
+    @book = current_user.books.build(book_params)
 
     if @book.save
       redirect_to root_path
